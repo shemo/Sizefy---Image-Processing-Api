a4349dd4c07483474de2ce6b42c4472c26ee552c
Shimaa Adel <eng.sh.adel@gmail.com>
2022-06-18 00:23:07 +0200

    add middleware logging, security

--- build/index.js
+++ build/index.js
@@ -4,12 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
 const PORT = 3000;
 //create instance of server
 const app = (0, express_1.default)();
-// add routing
+//request logger middleware
+app.use((0, morgan_1.default)('common'));
+//http security middleware
+app.use((0, helmet_1.default)());
+// add routing for / path
 app.get('/', (req, res) => {
     res.json({ message: 'Hello' });
+});
+//post request
+app.post('/', (req, res) => {
+    res.json({ message: 'Hello from post', data: req.body });
 });
 // start express server
 app.listen(PORT, () => {

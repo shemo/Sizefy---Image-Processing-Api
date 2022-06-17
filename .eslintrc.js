6392448f6fede25fb8159233f2a7a15a8cf1e7d3
Shimaa Adel <eng.sh.adel@gmail.com>
2022-06-17 02:30:06 +0200

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2,
+    semi: ['error', 'never'],
+    'no-console': 1, //warning
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+}
